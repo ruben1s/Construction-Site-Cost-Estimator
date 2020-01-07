835ecad3dc67ba58488c64a4636c048041afa09e
Ruben Sanchez <33906590+ruben1s@users.noreply.github.com>
2020-01-06 17:51:43 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const asphault = 140;
-const dirt = 100;
-const sand = 125;
 const ton = 2000;
 
-let width = document.getElementById("width").value;
-let length = document.getElementById("length").value;
-let depth = document.getElementById("depth").value;
+let width = document.getElementById("width");
+let length = document.getElementById("length");
+let depth = document.getElementById("depth");
+let materialDropdown = document.getElementById("myDropdown");
+let option = getVal;
 
-const calculateBtn = document.getElementById('calculateBtn');
-const calculate = () => {
-    let volume = width * length * depth;
-    let total = (volume * asphault) / ton;
-    console.log(`The total cost is \$${total} for the project.`);
+if (option) {
+    option = asphault;
 }
 
-const calculateEventHandler = calculateBtn.addEventListener('click', calculate);
+function getVal() {
+    option = materialDropdown.options[materialDropdown.selectedIndex].value;
+    console.log(option);
+}
 
+function calculateInput() {
+    let wid = parseFloat(width.value);
+    let len = parseFloat(length.value);
+    let dep = parseFloat(depth.value);
+    let opt = option;
+    if (wid >= 0 && len >= 0 && dep >= 0) {
+    let volume = wid * len * dep;
+    let total = (volume * opt) / ton;
+    total = parseFloat(total).toFixed(2);
+    
+    let newText = document.createTextNode(`The total weight is ${total} tons for the project.`),
+    h1 = document.getElementById("results");
+    
+    h1.appendChild(newText);
+
+    } else {
+        alert('Please enter values greater than 0.');
+    }
+}
+
+const calculateEventHandler = calculateBtn.addEventListener('click', calculateInput);
+
+const selectedInputEventHandler = materialDropdown.addEventListener('click', getVal);
